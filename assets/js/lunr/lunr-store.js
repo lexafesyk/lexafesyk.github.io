1dcb58915c1f22fcdbdf3f10828d8e73d8feb7ab
lexafesyk <lexafesyk@users.noreply.github.com>
2024-10-25 18:12:23 +0000

    deploy: 970bab196a1f5471a451165c8b7cd83877b9df0f

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Post 1",
+        "excerpt":"Post 1 Header   Post 1 Content  ","categories": [],
+        "tags": ["tag1","tag2"],
+        "url": "/2024/09/24/post-1.html",
+        "teaser": null
+      },{
+        "title": "Post 2",
+        "excerpt":"Post 2 Header   Post 2 Content  ","categories": [],
+        "tags": ["tag1","tag3"],
+        "url": "/2024/10/21/post-2.html",
+        "teaser": null
+      }]
